6b75429a6897059609205f46aa0d7bc27cc57f62
shfnr <145088388+shfnr@users.noreply.github.com>
2025-09-11 17:40:47 +0800

    Delete week01-2b.py

--- week01-2b.py
+++
@@ -1,6 +0,0 @@
-#week01-2b.py
-#LeeCode 28.:Find the Index of the Firth Occurence in a String
-#在 haystack 乾稻草堆裡找到needle針(大海撈針)
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
