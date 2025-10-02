c8cc688f232e8354bf87511239b4c11491916284
shfnr <145088388+shfnr@users.noreply.github.com>
2025-10-02 14:51:41 +0800

    Delete week03-3a.c

--- week03-3a.c
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=0)ans.push_back(nums[i]);
-        }
-    }
-};
