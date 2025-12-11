c7a3cbd2b4512877855c75df8442628d0463dd07
shfnr <145088388+shfnr@users.noreply.github.com>
2025-12-11 14:38:13 +0800

    week06

---
+++ week06/week06-4.cpp
@@ -0,0 +1,21 @@
+// week06-4.cpp
+// LeetCode學習計畫 Simulation 模擬 第2題 657. Robot Return to Origin
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0,y = 0; // 一開始在原點 0,Origin
+        for(char c : moves){ //C++進階迴圈
+            if(c=='U'){ //往上
+                y++;
+            }else if(c=='D'){ //往下
+                y--;
+            }else if(c=='L'){ //往左
+                x--;
+            }else if(c=='R'){ //往右
+                x++;
+            }
+        }// 最後還有留在原點(0,0) 嗎?
+        if(x==0 && y==0) return true;
+        else return false;
+    }
+};
